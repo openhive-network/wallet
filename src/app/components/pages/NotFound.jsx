c51d550cb8012965d220ff41301138f5fda98e0d
Evan Chou <chou86.e@gmail.com>
2020-03-25 14:15:00 +0000

    fix notfound page

--- src/app/components/pages/NotFound.jsx
+++ src/app/components/pages/NotFound.jsx
@@ -11,12 +11,9 @@ class NotFound extends React.Component {
                         <div className="top-bar-left">
                             <ul className="menu">
                                 <li className="Header__top-logo">
-                                    <Link to="/">
+                                    <a href="/">
                                         <HiveLogo />
-                                    </Link>
-                                </li>
-                                <li className="Header__top-hive show-for-medium noPrint">
-                                    <a href="/">hive</a>
+                                    </a>
                                 </li>
                             </ul>
                         </div>
@@ -24,7 +21,6 @@ class NotFound extends React.Component {
                 </div>
                 <div className="NotFound float-center">
                     <div>
-                        <Icon name="steem" size="4x" />
                         <h4 className="NotFound__header">
                             Sorry! This page doesn't exist.
                         </h4>
@@ -32,25 +28,8 @@ class NotFound extends React.Component {
                             Not to worry. You can head back to{' '}
                             <a style={{ fontWeight: 800 }} href="/">
                                 our homepage
-                            </a>, or check out some great posts.
+                            </a>.
                         </p>
-                        <ul className="NotFound__menu">
-                            <li>
-                                <a href="/created">new posts</a>
-                            </li>
-                            <li>
-                                <a href="/hot">hot posts</a>
-                            </li>
-                            <li>
-                                <a href="/trending">trending posts</a>
-                            </li>
-                            <li>
-                                <a href="/promoted">promoted posts</a>
-                            </li>
-                            <li>
-                                <a href="/active">active posts</a>
-                            </li>
-                        </ul>
                     </div>
                 </div>
             </div>
