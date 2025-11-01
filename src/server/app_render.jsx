9b39c1ff66ccc405ed4608577be380fa6626f914
Gandalf <gandalf@storm.pl>
2025-11-01 12:58:28 +0000

    Use English as default locale for anonymous users

--- src/server/app_render.jsx
+++ src/server/app_render.jsx
@@ -35,7 +35,13 @@ async function appRender(ctx, locales = false, resolvedAssets = false) {
             }
         }
         if (!userPreferences.locale) {
-            let locale = ctx.getLocaleFromHeader();
+            if (ctx.session.a) {
+                // Logged-in user: use Accept-Language header as before
+                locale = ctx.getLocaleFromHeader();
+            } else {
+                // Anonymous user: ignore header and default to English
+                locale = 'en';
+            }
             if (locale) locale = locale.substring(0, 2);
             const supportedLocales = locales ? locales : getSupportedLocales();
             const localeIsSupported = supportedLocales.find(l => l === locale);
